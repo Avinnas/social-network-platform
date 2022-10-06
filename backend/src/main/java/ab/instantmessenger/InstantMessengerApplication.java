320c5d1c92513026250c39596d044eef6665fefd
xAriess <baranadrian98@gmail.com>
2022-10-06 20:23:27 +0200

    Initial backend commit

---
+++ backend/src/main/java/ab/instantmessenger/InstantMessengerApplication.java
@@ -0,0 +1,13 @@
+package ab.instantmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantMessengerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ab/instantmessenger/InstantMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package ab.instantmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
